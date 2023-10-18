00a5d2278afd5b2b950db4875638e3242fac7b73
zikotan <tananzakaria630@gmail.com>
2023-10-18 11:08:32 -0700

    response

---
+++ lineHandling.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * execLine - the execLine function
+ * @cmd: command
+ * @argv: argument value
+ *
+ * Return: Integer.
+ */
+int execLine(char **cmd, char **argv)
+{
+	pid_t childProc;
+	int stat;
+
+	childProc = fork();
+	if (childProc == 0)
+	{
+		if (execve(cmd[0], cmd, environ) == -1)
+		{
+			perror(argv[0]);
+			free2DArray(cmd);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(childProc, &stat, 0);
+		free2DArray(cmd);
+	}
+	return (WEXITSTATUS(stat));
+}
+
+/**
+ * readLine - the readLine function
+ *
+ * Return: String.
+ */
+char *readLine(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t nread;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+	nread = getline(&line, &size, stdin);
+	if (nread == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+/**
+ * splitLine - the splitLine function
+ * @line: the line
+ *
+ * Return: String.
+ */
+
+char **splitLine(char *line)
+{
+	char *split = NULL, *help = NULL, **cmd = NULL;
+	int count = 0, i = 0;
+
+	if (!line)
+		return (NULL);
+	help = duplicate(line);
+	split = strtok(help, LIM);
+	if (split == NULL)
+	{
+		free(line);
+		line = NULL;
+		free(help);
+		help = NULL;
+		return (NULL);
+	}
+	while (split)
+	{
+		count++;
+		split = strtok(NULL, LIM);
+	}
+	free(help), help = NULL;
+	cmd = malloc(sizeof(char *) * (count + 1));
+	if (!cmd)
+	{
+		free(line), line = NULL;
+		return (NULL);
+	}
+	split = strtok(line, LIM);
+	while (split)
+	{
+		cmd[i] = duplicate(split);
+		split = strtok(NULL, LIM);
+		i++;
+	}
+	free(line), line = NULL;
+	cmd[i] = NULL;
+	return (cmd);
+}
---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#define LIM " \t\n"
+
+extern char **environ;
+char *findEnv(char *arg);
+char *findPath(char *cmd);
+
+char *intToAscii(int a);
+void strRev(char *ch, int s);
+void free2DArray(char **arrays);
+void myError(char *id, char *cmd, int i);
+
+char *readLine(void);
+char **splitLine(char *line);
+int execLine(char **cmd, char **argv);
+
+char *duplicate(const char *dup);
+int compare(char *ch1, char *ch2);
+int length(char *ch);
+char *catt(char *to, char *from);
+char *coppy(char *to, char *from);
+
+#endif
