67e8f30d1d3e2576159b7f5717020b060dd853fe
zikotan <tananzakaria630@gmail.com>
2023-10-18 04:42:09 -0700

    response

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - the main function
+ * @argc: argument counter
+ * @argv: argument value
+ *
+ * Return: always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *line = NULL, **cmd = NULL;
+	int stat = 0, ind = 0;
+	(void) argc;
+
+	while (1)
+	{
+		line = readLine();
+		if (line == NULL)
+		{
+			if (isatty(DTDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (stat);
+		}
+		ind++;
+		cmd = splitLine(line);
+		if (!cmd)
+			continue;
+		stat = execLine(cmd, agrv);
+	}
+}
