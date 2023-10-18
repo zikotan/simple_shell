84f4b85b5cd43e544a1a2dccd911ca3384ca7c3b
zikotan <tananzakaria630@gmail.com>
2023-10-18 11:47:46 -0700

    response

--- utils.c
+++ utils.c
@@ -21,3 +21,73 @@ void free2DArray(char **arrays)
 	free(arrays);
 	arrays = NULL;
 }
+
+/**
+ * myError - the myError function
+ * @id: the name
+ * @cmd: the command
+ * @i: the index
+ *
+ * Return: Nothing.
+ */
+void myError(char *id, char *cmd, int i)
+{
+	char *ind, err[] = ": not found\n";
+
+	ind = intToAscii(i);
+	write(STDERR_FILENO, id, length(id));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, ind, length(ind));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, cmd, length(cmd));
+	write(STDERR_FILENO, err, length(err));
+	free(ind);
+}
+
+/**
+ * intToAscii - the intToAscii function
+ * @a: the integer
+ *
+ * Return: String.
+ */
+char *intToAscii(int a)
+{
+	char buff[15];
+	int i = 0;
+
+	if (a == 0)
+		buff[i++] = '0';
+	else
+	{
+		while (a > 0)
+		{
+			buff[i++] = (a % 10) + '0';
+			a /= 10;
+		}
+	}
+	buff[i] = '\0';
+	strRev(buff, i);
+	return (duplicate(buff));
+}
+
+/**
+ * strRev - the strRev function
+ * @ch: the string
+ * @s: the size
+ *
+ * Return: Nothing.
+ */
+void strRev(char *ch, int s)
+{
+	char help;
+	int begin = 0, stop = s - 1;
+
+	while (begin < stop)
+	{
+		help = ch[begin];
+		ch[begin] = ch[stop];
+		ch[stop] = help;
+		begin++;
+		stop--;
+	}
+}
